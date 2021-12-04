ac035d2e31e880c5b4cae119b61b73e1392b7d78
Luke Diebold <lukediebold@agripath.com.au>
2021-12-04 21:12:20 +1100

    WIP

--- core/src/Contracts/UseIdentityPasswordRegister.ts
+++ core/src/Contracts/UseIdentityPasswordRegister.ts
@@ -11,6 +11,7 @@ export interface IdentityPasswordRegisterForm {
 
 export interface UseIdentityPasswordRegisterReturn {
   form: Ref<IdentityPasswordRegisterForm>;
+  additionalFormFields?: Ref<Record<number | string, unknown>>;
   register: () => Promise<void>;
   loading: Ref<boolean>;
   validationErrors: Ref<ValidationErrors>;
