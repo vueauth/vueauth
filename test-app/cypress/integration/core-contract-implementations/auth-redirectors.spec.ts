73b55b669182b0075ffdef4b1ac8002b3c4d8310
Luke Diebold <luke@ldiebold.com>
2022-03-18 11:32:16 +1000

    remove comments from test files

--- test-app/cypress/integration/core-contract-implementations/auth-redirectors.spec.ts
+++ test-app/cypress/integration/core-contract-implementations/auth-redirectors.spec.ts
@@ -1,15 +1,5 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
 import getProviders from "../../../getProviders"
 
 const providers = getProviders()
