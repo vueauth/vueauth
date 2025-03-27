b4b13c51295f75f9cf36d1c978090c1df4b250ec
Luke Diebold <luke@ldiebold.com>
2025-03-27 14:22:12 +1100

    more versioning work

--- quasar-app-extension-auth/app-extension/src/providerDependencies.js
+++ quasar-app-extension-auth/app-extension/src/providerDependencies.js
@@ -2,28 +2,33 @@ export default {
   firebase: {
     dependencies: {
       firebase: '^9.0.0',
-      '@vueauth/firebase': '^1.1.0-alpha.3'
+      '@vueauth/firebase': '^1.1.0-alpha.3',
+      '@vueauth/core': '^1.1.0-alpha.*'
     }
   },
   supabase: {
     dependencies: {
       '@supabase/supabase-js': '^1.28.5',
-      '@vueauth/supabase': '^1.1.0-alpha.3'
+      '@vueauth/supabase': '^1.1.0-alpha.3',
+      '@vueauth/core': '^1.1.0-alpha.*'
     }
   },
   indexeddb: {
     dependencies: {
-      '@vueauth/indexeddb': '^1.1.0-alpha.6'
+      '@vueauth/indexeddb': '^1.1.0-alpha.6',
+      '@vueauth/core': '^1.1.0-alpha.*'
     }
   },
   sanctum: {
     dependencies: {
-      '@vueauth/sanctum': '^1.1.0-alpha.3'
+      '@vueauth/sanctum': '^1.1.0-alpha.3',
+      '@vueauth/core': '^1.1.0-alpha.*'
     }
   },
   strapi: {
     dependencies: {
-      '@vueauth/strapi': '^1.1.0-alpha.3'
+      '@vueauth/strapi': '^1.1.0-alpha.3',
+      '@vueauth/core': '^1.1.0-alpha.*'
     }
   }
 }
