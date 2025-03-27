6dc8ba325c252c9f23c1487d7b8e762e0db48cc9
Luke Diebold <luke@ldiebold.com>
2025-03-27 14:11:28 +1100

    correct alpha sub versioning

--- quasar-app-extension-auth/app-extension/src/providerDependencies.js
+++ quasar-app-extension-auth/app-extension/src/providerDependencies.js
@@ -2,28 +2,28 @@ export default {
   firebase: {
     dependencies: {
       firebase: '^9.0.0',
-      '@vueauth/firebase': '^1.1.0-alpha.2'
+      '@vueauth/firebase': '^1.1.0-alpha.3'
     }
   },
   supabase: {
     dependencies: {
       '@supabase/supabase-js': '^1.28.5',
-      '@vueauth/supabase': '^1.1.0-alpha.2'
+      '@vueauth/supabase': '^1.1.0-alpha.3'
     }
   },
   indexeddb: {
     dependencies: {
-      '@vueauth/indexeddb': '^1.1.0-alpha.5'
+      '@vueauth/indexeddb': '^1.1.0-alpha.6'
     }
   },
   sanctum: {
     dependencies: {
-      '@vueauth/sanctum': '^1.1.0-alpha.2'
+      '@vueauth/sanctum': '^1.1.0-alpha.3'
     }
   },
   strapi: {
     dependencies: {
-      '@vueauth/strapi': '^1.1.0-alpha.2'
+      '@vueauth/strapi': '^1.1.0-alpha.3'
     }
   }
 }
