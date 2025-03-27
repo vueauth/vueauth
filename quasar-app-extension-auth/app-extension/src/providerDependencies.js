8b73055f908aeb72ce7dc267b24f68cfdac7a88b
Luke Diebold <luke@ldiebold.com>
2025-03-27 14:05:49 +1100

    bump provider deps for ae

--- quasar-app-extension-auth/app-extension/src/providerDependencies.js
+++ quasar-app-extension-auth/app-extension/src/providerDependencies.js
@@ -2,28 +2,28 @@ export default {
   firebase: {
     dependencies: {
       firebase: '^9.0.0',
-      '@vueauth/firebase': '^1.1.0-alpha.1'
+      '@vueauth/firebase': '^1.1.0-alpha.2'
     }
   },
   supabase: {
     dependencies: {
       '@supabase/supabase-js': '^1.28.5',
-      '@vueauth/supabase': '^1.1.0-alpha.1'
+      '@vueauth/supabase': '^1.1.0-alpha.2'
     }
   },
   indexeddb: {
     dependencies: {
-      '@vueauth/indexeddb': '^1.1.0-alpha.4'
+      '@vueauth/indexeddb': '^1.1.0-alpha.5'
     }
   },
   sanctum: {
     dependencies: {
-      '@vueauth/sanctum': '^1.1.0-alpha.1'
+      '@vueauth/sanctum': '^1.1.0-alpha.2'
     }
   },
   strapi: {
     dependencies: {
-      '@vueauth/strapi': '^1.1.0-alpha.1'
+      '@vueauth/strapi': '^1.1.0-alpha.2'
     }
   }
 }
