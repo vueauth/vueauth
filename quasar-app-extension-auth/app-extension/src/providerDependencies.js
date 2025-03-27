90da988c6e3ee5e8316a705ba1b4a679cb61cd59
Luke Diebold <luke@ldiebold.com>
2025-03-27 21:18:14 +1100

    fix invalid dep in ae

--- quasar-app-extension-auth/app-extension/src/providerDependencies.js
+++ quasar-app-extension-auth/app-extension/src/providerDependencies.js
@@ -2,33 +2,33 @@ export default {
   firebase: {
     dependencies: {
       firebase: '^9.0.0',
-      '@vueauth/firebase': '^1.1.0-alpha.3',
-      '@vueauth/core': '^1.1.0-alpha.*'
+      '@vueauth/firebase': '^1.1.0-alpha.4',
+      '@vueauth/core': '1.1.0-alpha.*'
     }
   },
   supabase: {
     dependencies: {
       '@supabase/supabase-js': '^1.28.5',
-      '@vueauth/supabase': '^1.1.0-alpha.3',
-      '@vueauth/core': '^1.1.0-alpha.*'
+      '@vueauth/supabase': '^1.1.0-alpha.4',
+      '@vueauth/core': '1.1.0-alpha.*'
     }
   },
   indexeddb: {
     dependencies: {
-      '@vueauth/indexeddb': '^1.1.0-alpha.6',
-      '@vueauth/core': '^1.1.0-alpha.*'
+      '@vueauth/indexeddb': '^1.1.0-alpha.7',
+      '@vueauth/core': '1.1.0-alpha.*'
     }
   },
   sanctum: {
     dependencies: {
-      '@vueauth/sanctum': '^1.1.0-alpha.3',
-      '@vueauth/core': '^1.1.0-alpha.*'
+      '@vueauth/sanctum': '^1.1.0-alpha.4',
+      '@vueauth/core': '1.1.0-alpha.*'
     }
   },
   strapi: {
     dependencies: {
-      '@vueauth/strapi': '^1.1.0-alpha.3',
-      '@vueauth/core': '^1.1.0-alpha.*'
+      '@vueauth/strapi': '^1.1.0-alpha.4',
+      '@vueauth/core': '1.1.0-alpha.*'
     }
   }
 }
